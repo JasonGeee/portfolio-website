b50c0b9b5c36038397f2db7009baeec5b1b24512
jasongutierrez <jasongeee@yahoo.com>
2023-07-12 06:36:50 -0500

    Added content to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,25 @@
+import { Box, Flex, HStack, Icon, Spacer, Text } from "@chakra-ui/react";
+import { CiLinkedin } from "react-icons/ci";
+import { BiLogoGithub } from "react-icons/bi";
+
 export default function Footer() {
-  return <footer>Footer Section</footer>;
+  return (
+    <Flex
+      width="100%"
+      justifyContent={"center"}
+      alignItems={"center"}
+      px={40}
+      py={5}
+      bgColor={"#d8eefe"}
+    >
+      <Text color="#094067" fontWeight={700}>
+        Copyright © 2023. All rights are reserved
+      </Text>
+      <Spacer />
+      <HStack>
+        <Icon as={CiLinkedin} boxSize={9} color="#094067" />
+        <Icon as={BiLogoGithub} boxSize={8} color="#094067" />
+      </HStack>
+    </Flex>
+  );
 }
