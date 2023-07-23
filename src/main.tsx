2be5b7073647ab536a8986a1164332bb3dbc8c98
jasongutierrez <jasongeee@yahoo.com>
2023-07-23 05:03:45 -0500

    Added Outfit Font from Google Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource-variable/outfit";
+
+//extends the theme with Font Outfit from Google Fonts
+const theme = extendTheme({
+  fonts: {
+    headings: `'Outfit Variable', sans-serif`,
+    body: `'Outfit Variable', sans-serif`,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
